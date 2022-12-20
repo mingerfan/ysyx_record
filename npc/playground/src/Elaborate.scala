6b3e2461827790292e1bfdd291e64c60573d7297
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-20 22:19:21 +0800

    > generate verilog ysyx_22041536 徐铭 Linux xs-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:19:21 up  3:56,  1 user,  load average: 0.35, 0.18, 0.13

--- npc/playground/src/Elaborate.scala
+++ npc/playground/src/Elaborate.scala
@@ -3,5 +3,5 @@ import chisel3._
 
 object Elaborate extends App {
   def top = new GCD()
-  emitVerilog(new top, Array("--target-dir", "./build"))
+  emitVerilog(top, Array("--target-dir", "./build"))
 }

e1f9f8bb71e3308ab524f8495f48736ac83d3172
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-21 12:11:23 +0800

    > generate verilog ysyx_22041536 徐铭 Linux xs-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:11:23 up  1:00,  1 user,  load average: 0.27, 0.20, 0.18

--- npc/playground/src/Elaborate.scala
+++ npc/playground/src/Elaborate.scala
@@ -2,5 +2,5 @@ import chisel3._
 
 object Elaborate extends App {
     val top = new HomeWorkHDL.top()
-    emitVerilog(top, Array("--target-dir", "build/top/"))
+    emitVerilog(top, Array("--target-dir", "./build/top"))
 }

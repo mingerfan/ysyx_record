4a550368394fb5b2265ee710ff38d8b7dc6ed1e8
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-22 14:58:49 +0800

    > generate verilog ysyx_22041536 徐铭 Linux xs-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:58:49 up  3:20,  1 user,  load average: 0.82, 0.49, 0.29

--- npc/playground/src/DigitCircuitExp/encoder/top.scala
+++ npc/playground/src/DigitCircuitExp/encoder/top.scala
@@ -5,25 +5,25 @@ import chisel3.util._
 class top extends Module {
     val io = IO(new Bundle {
         val sw = Input(UInt(9.W))
-        val led = Output(UInt(3.W))
+        val led = Output(UInt(4.W))
         val seg = Output(UInt(7.W))
         val unused = Output(UInt(2.W))
     })
-    
+
     // 为了防止verilator仿真报UNUSED，添加时序逻辑
     val un_reg = RegInit(2.U)
     un_reg := un_reg + 1.U
     io.unused := un_reg
 
-    val enc_in = Wire(UInt(8.W))
+    val enc_in = Wire(UInt(7.W))
     enc_in := io.sw(7, 0)
 
     var last_mux = 0.U(3.W)
     for (i <- 0 until 8) {
         last_mux = Mux(enc_in(i)===1.U, i.U, last_mux)
     }
-    val enc_result = Mux(io.sw(0), last_mux, 0.U)
-    io.led := Cat(io.sw(0), enc_result)
+    val enc_result = Mux(io.sw(8), last_mux, 0.U)
+    io.led := Cat(io.sw(8), enc_result)
 
     io.seg := 0.U
     switch (enc_result) {

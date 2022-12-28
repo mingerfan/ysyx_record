cb73b5a2cf4abf434572fbfbc67adb6b8c3e8588
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-28 18:32:45 +0800

    > generate verilog ysyx_22041536 徐铭 Linux xs-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:32:45 up 53 min,  1 user,  load average: 0.16, 0.14, 0.07

--- npc/playground/src/DigitCircuitExp/RegAndMemory/top.scala
+++ npc/playground/src/DigitCircuitExp/RegAndMemory/top.scala
@@ -5,9 +5,14 @@ import chisel3.util._
 class top extends Module {
     val io = IO(new Bundle {
         val addr = Input(UInt(4.W))
+        val in = Input(UInt(4.W))
+        val we = Input(Bool())
         val out = Output(UInt(4.W))
     })
 
     val regs = RegInit(VecInit(Seq.fill(16)(0.U(4.W))))
+    when (io.we){
+        regs(io.addr) := io.in
+    }
     io.out := regs(io.addr)
 }

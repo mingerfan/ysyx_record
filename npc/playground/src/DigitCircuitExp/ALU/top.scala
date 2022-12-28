322becfe91feba9adbd06c44c87c70ea4b4a113d
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-28 13:32:12 +0800

    > generate verilog ysyx_22041536 徐铭 Linux xs-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:32:12 up 5 min,  1 user,  load average: 0.16, 0.21, 0.11

--- npc/playground/src/DigitCircuitExp/ALU/top.scala
+++ npc/playground/src/DigitCircuitExp/ALU/top.scala
@@ -54,7 +54,7 @@ class AddSub(width: Int) extends aluAbs(width) {
     carry_u := res(width)
     io.out := res(width-1, 0)
     exio.zero := io.out(width-2, 0) === 0.U
-    exio.overflow := (io.A(width-1) === B_(width-1)) && (io.A(width-1) =/= io.out(width-1))
+    exio.overflow := 1.U
     exio.carry := carry_u === 1.U
 }
  

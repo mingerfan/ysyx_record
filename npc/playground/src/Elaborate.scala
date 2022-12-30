2ef9a4e65311f06fd271c57626e28de392108159
ysyx_22041536-Ming Xu <xumingxs@qq.com>
2022-12-30 23:19:15 +0800

    zyhssb hhhhhW

--- npc/playground/src/Elaborate.scala
+++ npc/playground/src/Elaborate.scala
@@ -2,6 +2,6 @@ import chisel3._
 import chisel3.util._
 
 object Elaborate extends App {
-    def top = new ShiftReg.top
+    def top = new DCE_Mux.top
     emitVerilog(top, Array("--target-dir", "./build"))
 }

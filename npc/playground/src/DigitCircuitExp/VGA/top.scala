105032bf25b94be1d1438f5fb51a9cb91e7f57ad
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-03 18:15:12 +0800

    > generate verilog ysyx_22041536 徐铭 Linux xs-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:15:12 up 33 min,  1 user,  load average: 0.58, 1.23, 1.01

--- npc/playground/src/DigitCircuitExp/VGA/top.scala
+++ npc/playground/src/DigitCircuitExp/VGA/top.scala
@@ -1,7 +1,6 @@
 package DCE_VGA
 import chisel3._
 import chisel3.util._
-import HomeWorkHDL.ClockGen
 
 class top extends Module {
     val io = IO(new Bundle {
@@ -14,9 +13,8 @@ class top extends Module {
     })
 
     val vgaCtrl = Module(new VGA_Ctrl)
-    val clockGen = Module(new ClockGen(100000000, 25000000))
-    vgaCtrl.io.pclk := clockGen.io.outClk.asClock
-    vgaCtrl.io.vgaData := "hFFFFFF".U
+    vgaCtrl.io.pclk := clock
+    vgaCtrl.io.vgaData := Mux(vgaCtrl.io.hAddr < 300, "hFFFFFF".U, "h3F8F5F".U)
     io.vgaHsync := vgaCtrl.io.hsync
     io.vgaVsync := vgaCtrl.io.vsync
     io.vgaBlank := vgaCtrl.io.valid

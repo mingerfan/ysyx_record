a0f9920a7836c0bd0137cfc536a25154a927ae9d
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-10 12:56:55 +0800

    > compile NEMU ysyx_22041536 徐铭 Linux xs-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:56:55 up  3:23,  1 user,  load average: 0.61, 0.30, 0.43

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -294,7 +294,7 @@ word_t eval(int p, int q) {
       case('+'): return val1 + val2;
       case('-'): return val1 - val2;
       case('*'): return val1 * val2;
-      case('/'): return val1 / val2;
+      case('/'): return (sword_t)val1 / (sword_t)val2;
       case(TK_EQ): return val1 == val2;
       case(TK_NEQ): return val1 != val2;
       case(AND): return val1 && val2;

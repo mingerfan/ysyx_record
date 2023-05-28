df98ced43ff58e0e92ba6599e51f8820e1b70057
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-28 19:19:17 +0800

    > sim RTL ysyx_22041536 徐铭 Linux xs-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:19:17 up  9:13,  1 user,  load average: 1.25, 0.56, 0.56

--- npc/playground/verilator_test/csrc/npc_memory.cpp
+++ npc/playground/verilator_test/csrc/npc_memory.cpp
@@ -28,7 +28,7 @@ paddr_t host_to_guset(uint8_t *addr) { return addr - pmem + CONFIG_NPC_PC; }
 
 static uint64_t pmem_read_base(paddr_t paddr, int len)
 {
-    Assert(0 >= 0x800000, "fas");
+    Assert(0 >= 0x800, "fas");
     Assert(paddr >= CONFIG_NPC_PC, "Addr is less than CONFIG_NPC_PC");
     return host_read(guest_to_host(paddr), len);
 }

da0907d9dc1ea52d6c05797667c4fdcc200264f1
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-29 17:40:13 +0800

    > generate verilog ysyx_22041536 徐铭 Linux xs-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:40:13 up  7:14,  1 user,  load average: 1.40, 0.47, 0.19

--- npc/playground/src/ScProcessor/MEMWR/memWr.scala
+++ npc/playground/src/ScProcessor/MEMWR/memWr.scala
@@ -40,16 +40,25 @@ class MEMWR extends Module {
 
     val align = taddr-taddr_t
     val word_mask = "b1111".U << align(4, 0)
-    val byte_mask = "b11".U << align(4, 0)
+    val hword_mask = "b11".U << align(4, 0)
+    val byte_mask = "b1".U << align(4, 0)
     
     val word_vec = Wire(Vec(2, Bool()))
     word_vec := VecInit((0 until 2).map(i => align === (i*4).U))
+    val hword_vec = Wire(Vec(4, Bool()))
+    hword_vec := VecInit((0  until 4).map(i => align === (i*2).U))
     val byte_vec = Wire(Vec(8, Bool()))
     byte_vec := VecInit((0 until 8).map(i => align === i.U))
 
     val word_data = Mux1H(Seq(
         word_vec(0) -> read_data(31, 0),
         word_vec(1) -> read_data(63, 32)
+    ))
+    val hword_data = Mux1H(Seq(
+        hword_vec(0) -> read_data(15, 0),
+        hword_vec(1) -> read_data(31, 16),
+        hword_vec(2) -> read_data(47, 32),
+        hword_vec(3) -> read_data(63, 48)
     ))
     val byte_data = Mux1H(Seq(
         byte_vec(0) -> read_data(7, 0),

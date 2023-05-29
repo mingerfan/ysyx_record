d1d8592e779324d44c36d29457c43107b3058834
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-29 14:02:40 +0800

    > generate verilog ysyx_22041536 徐铭 Linux xs-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:02:40 up  3:37,  1 user,  load average: 1.74, 0.67, 0.47

--- npc/playground/verilator_test/csrc/dpi_wrapper.cpp
+++ npc/playground/verilator_test/csrc/dpi_wrapper.cpp
@@ -21,6 +21,7 @@ void set_state_end_(unsigned long long c_pc, int halt_ret)
 void pmem_read(long long raddr, long long *rdata) {
     if (npc_state.state == NPC_RUNNING) {
         // 总是读取地址为`raddr & ~0x7ull`的8字节返回给`rdata`
+        G_DEBUG_I("raddr: 0x%016x", raddr);
         *rdata = paddr_read(raddr & ~0x7ull, 8);
     } else {
         *rdata = 0;
@@ -32,6 +33,7 @@ void pmem_write(long long waddr, long long wdata, char wmask) {
         // 总是往地址为`waddr & ~0x7ull`的8字节按写掩码`wmask`写入`wdata`
         // `wmask`中每比特表示`wdata`中1个字节的掩码,
         // 如`wmask = 0x3`代表只写入最低2个字节, 内存中的其它字节保持不变
+        G_DEBUG_I("waddr: 0x%016x wmask:0x%016x", waddr, wmask);
         uint64_t val = paddr_read(waddr & ~0x7ull, 8);
         uint64_t res = 0;
         for (int i = 0; i < 8; i++) {

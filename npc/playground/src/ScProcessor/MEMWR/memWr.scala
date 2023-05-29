fe58314671b6636ac6ece11e7951760346dd423b
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-29 20:00:03 +0800

    > generate verilog ysyx_22041536 徐铭 Linux xs-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:00:03 up  9:34,  1 user,  load average: 1.14, 0.47, 0.34

--- npc/playground/src/ScProcessor/MEMWR/memWr.scala
+++ npc/playground/src/ScProcessor/MEMWR/memWr.scala
@@ -91,7 +91,8 @@ class MEMWR extends Module {
         hit("sh") -> hword_mask,
         hit("sb") -> byte_mask,
         hit("ld") -> 0.U,
-        hit("lw") -> 0.U
+        hit("lw") -> 0.U,
+        hit("lbu")-> 0.U
     ))
     mem_wr_in.io.en := io.memOps.asTypeOf(Vec(OPS_NUM, Bool())).reduceTree(_ | _)
 }

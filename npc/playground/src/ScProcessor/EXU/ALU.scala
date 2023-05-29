043475d57684ea5f9e07db62196285025ed13abf
ysyx_22041536-Ming Xu <xumingxs@qq.com>
2023-05-29 16:36:56 +0800

    fix(ALU): UCMP bug--should not use sub

--- npc/playground/src/ScProcessor/EXU/ALU.scala
+++ npc/playground/src/ScProcessor/EXU/ALU.scala
@@ -25,7 +25,7 @@ class ALU extends Module {
 
     io.out := Mux1H(Seq(
         hit("SUM") -> (in1pin2),
-        hit("UCMP")-> (in1sin2.asSInt < 0.S),
+        hit("UCMP")-> (io.in1 < io.in2),
         hit("WSUM")-> (Fill(topInfo.XLEN - 11, in1pin2(31)) ## 
         in1pin2(30, 0)),
         hit("SUB") -> (in1sin2)

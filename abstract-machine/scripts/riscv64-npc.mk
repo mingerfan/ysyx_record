9ae70ea2c8f5cb7af59dfbee576434824d039a2a
tracer-ysyx2204 <tracer@ysyx.org>
2023-06-02 12:42:43 +0800

    > sim RTL ysyx_22041536 徐铭 Linux xs-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:42:43 up  3:20,  1 user,  load average: 0.07, 0.18, 0.24

--- abstract-machine/scripts/riscv64-npc.mk
+++ abstract-machine/scripts/riscv64-npc.mk
@@ -25,13 +25,13 @@ image: $(IMAGE).elf
 IMG_BIN = $(IMAGE).bin
 IMG_ELF = $(IMAGE).elf
 ARGS := --diff=$(NEMU_HOME)/build/riscv64-nemu-interpreter-so
-ARGS_B := $(ARGS) -b
+ARGS_B := --diff=$(NEMU_HOME)/build/riscv64-nemu-interpreter-so --batch
 
 run: image
 	$(MAKE) -C $(NPC_HOME) sim ARGS=$(ARGS) IMG_BIN=$(IMG_BIN) IMG_ELF=$(IMG_ELF)
 
 brun: image
-	$(MAKE) -C $(NPC_HOME) sim ARGS=$(ARGS_B) IMG_BIN=$(IMG_BIN) IMG_ELF=$(IMG_ELF)
+	$(MAKE) -C $(NPC_HOME) sim -- ARGS=$(ARGS_B) IMG_BIN=$(IMG_BIN) IMG_ELF=$(IMG_ELF)
 
 gdb: image
 	$(MAKE) -C $(NPC_HOME) gdb ARGS=$(ARGS) IMG_BIN=$(IMG_BIN) IMG_ELF=$(IMG_ELF)

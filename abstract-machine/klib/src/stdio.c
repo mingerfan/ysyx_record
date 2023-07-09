166939e9df3d78165a950835e84d6b617d9ddf95
ysyx_22041536-Ming Xu <xumingxs@qq.com>
2023-07-09 21:27:56 +0800

    feat: impl printf

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -6,9 +6,61 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+// typedef void (*output_fn)(char c, void *data);
+
+// int vformat(output_fn output, void *data, const char *format, ...) {
+
+// }
+
 int printf(const char *fmt, ...) {
-  putstr("Not implemented\n");
-  return 0;
+  va_list ap;
+  prtDes pspec;
+  int state = 0;
+  const char *ftmp = fmt;
+  char tmp[35];
+
+  int d;
+  char *s;
+  int cnt = 0;
+
+  va_start(ap, fmt);
+  while (*ftmp != '\0') {
+    state = 0;
+    switch (*ftmp) {
+      case '%':
+      ftmp = PrintDesDec(&pspec, ftmp);
+      state = 1;
+      break;
+
+      default:
+      cnt++;
+      putch(*ftmp++);
+    }
+    if (state == 1) {
+      switch (pspec.Spec) {
+        case PRINT_FAILED:
+        assert(0);
+        break;
+
+        case PRINT_INT:
+        d = va_arg(ap, int);
+        for (char *i = tmp; i != Int2Char(tmp, d); i++) {
+          if (*i != '\0') {
+            putch(*i);
+            cnt++;
+          }
+        }
+        break;
+
+        case PRINT_STR:
+        s = va_arg(ap, char*);
+        while (*s != '\0') { putch(*s); cnt++; s++; }
+        break;
+      }
+    }
+  }
+  va_end(ap);
+  return cnt;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {

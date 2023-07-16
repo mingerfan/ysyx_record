3ea12e00f4c74cb83493d466c0065cc26b53248e
ysyx_22041536-Ming Xu <xumingxs@qq.com>
2023-07-16 13:20:36 +0800

    feat($keyborad): impl key input

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -1,9 +1,16 @@
 #include <am.h>
 #include <nemu.h>
+#include <riscv/riscv.h>
 
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = 0;
-  kbd->keycode = AM_KEY_NONE;
+  uint32_t kbd_raw = inl(KBD_ADDR);
+  if ((kbd_raw & KEYDOWN_MASK) == KEYDOWN_MASK) {
+    kbd->keydown = true;
+    kbd->keycode = kbd_raw & ~KEYDOWN_MASK;
+  } else {
+    kbd->keydown = false;
+    kbd->keycode = AM_KEY_NONE;
+  }
 }

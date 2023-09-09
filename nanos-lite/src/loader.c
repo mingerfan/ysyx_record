8e4a45790853c6d9663ae5385343d0c43b01a841
ysyx_22041536-Ming Xu <xumingxs@qq.com>
2023-09-09 22:00:36 +0800

    feat($nanos): impl elf loader

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,5 +1,8 @@
 #include <proc.h>
 #include <elf.h>
+#include <stdio.h>
+#include <common.h>
+
 
 #ifdef __LP64__
 # define Elf_Ehdr Elf64_Ehdr
@@ -9,14 +12,44 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
+#if defined(__ISA_AM_NATIVE__)
+#define EXPECT_TYPE EM_X86_64
+#elif defined(__ISA_RISCV64__)
+#define EXPECT_TYPE EM_RISCV
+#else 
+#error Unsupported ISA __ISA__
+#endif
+
+
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
+size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
-  return 0;
+  Elf_Ehdr hd;
+  Elf_Phdr pd;
+  ramdisk_read(&hd, 0, sizeof(Elf_Ehdr));
+  assert((*(uint32_t*)hd.e_ident) == 0x464c457f);
+  assert(hd.e_machine == EM_RISCV);
+  uint64_t phoff = hd.e_phoff;
+  uint64_t phnum = hd.e_phnum;
+
+
+  for (int i = 0; i < phnum; i++) {
+    ramdisk_read(&pd, phoff + i * sizeof(Elf_Phdr), sizeof(Elf_Phdr));
+    if (pd.p_type != PT_LOAD) {
+      continue;
+    }
+    assert(pd.p_filesz <= pd.p_memsz);
+    ramdisk_read((void*)pd.p_vaddr, pd.p_offset, pd.p_filesz);
+    memset((void*)(pd.p_vaddr + pd.p_filesz), 0, pd.p_memsz - pd.p_filesz);
+  }
+
+  return hd.e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
   uintptr_t entry = loader(pcb, filename);
-  Log("Jump to entry = %p", entry);
+  Log("Jump to entry = %p", (void*)entry);
   ((void(*)())entry) ();
 }
 

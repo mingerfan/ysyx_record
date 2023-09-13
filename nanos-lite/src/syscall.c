f60c121060f5e5e95954323f3ed22fa790d30967
ysyx_22041536-Ming Xu <xumingxs@qq.com>
2023-09-13 15:18:23 +0800

    feat($strace): impl strace

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,9 +1,26 @@
 #include <common.h>
 #include "syscall.h"
+#define ENABLE_STRACE 1
+#define STRACE() strace(c, __func__);
+#define fn(x) static inline void x##_(Context *c)
+#define call(x) x##_(c)
 
-static void SYS_yield_(Context *c) {
+static inline void strace(Context *c, const char *name) {
+#if ENABLE_STRACE
+  printf("Current syscall is %s\n", name);
+#endif
+}
+
+fn(SYS_yield) {
+  STRACE();
   yield();
   c->GPRx = 0;
+}
+
+fn(SYS_exit) {
+  STRACE();
+  halt(0);
+  // it seems no necessity to give return value
 }
 
 void do_syscall(Context *c) {
@@ -11,8 +28,8 @@ void do_syscall(Context *c) {
   a[0] = c->GPR1;
 
   switch (a[0]) {
-    case SYS_yield: SYS_yield_(c); break;
-    case SYS_exit : halt(0); break;
+    case SYS_yield: call(SYS_yield); break;
+    case SYS_exit : call(SYS_exit); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }

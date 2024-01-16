a0bcc18680050f8bc410308fda6424e6ffcf441b
ysyx_22041536-Ming Xu <xumingxs@qq.com>
2024-01-16 20:25:40 +0800

    实现NDL的始终，修改timer-test，通过测试

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -1,18 +1,20 @@
+#include <stdint.h>
 #include <sys/time.h>
 #include <string.h>
 #include <assert.h>
 #include <stdio.h>
+#include <NDL.h>
 
 int main() {
-    struct timeval tv, last_tv;
-    assert(gettimeofday(&tv, (void*)0) == 0);
-    memcpy(&last_tv, &tv, sizeof(struct timeval));
+    uint32_t t, last_t;
+    t = NDL_GetTicks();
+    last_t = t;
     for (int i = 0; i < 10; i++) {
-        printf("sec: %ld, usec: %ld\n", tv.tv_sec, tv.tv_usec);
+        printf("curtime: %d\n", t);
         while (1) {
-            assert(gettimeofday(&tv, (void*)0) == 0);
-            if ((tv.tv_sec - last_tv.tv_sec)*1000 + (tv.tv_usec - last_tv.tv_usec)/1000 >= 500) {
-                memcpy(&last_tv, &tv, sizeof(struct timeval));
+            t = NDL_GetTicks();
+            if (t-last_t >= 500) {
+                last_t = t;
                 break;
             }
         }

95d81972f47554a27677228bab741aefa2cfc9c9
ysyx_22041536-Ming Xu <xumingxs@qq.com>
2024-02-05 14:58:11 +0800

    添加GenMask的utils函数（尚未测试）

--- npc/playground/src/ScProcessor/utils/bitUtils.scala
+++ npc/playground/src/ScProcessor/utils/bitUtils.scala
@@ -19,6 +19,18 @@ object bitUtils {
         ~((1<<low)-1) & x & ((1<<(high+1))-1)
     }
 
+
+    def GenMask(high: Int, low: Int) = {
+        require(low >= 0);
+        require(low < high);
+        (VecInit(List.fill(high+1)(true.B)).asUInt >> low  << low).asUInt
+    }
+
+    def GenMask(pos: Int) = {
+        require(pos >= 0);
+        1.U << pos
+    }
+
     // select particular bits, like x[high:low] in verilog
     // 0 to 63
     def BITS(x: BigInt, high: Int, low: Int) = {

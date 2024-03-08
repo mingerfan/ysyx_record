5c8d2d1b9cf0ea22cfa858f781ad3833c67977a5
ysyx_22041536-Ming Xu <xumingxs@qq.com>
2024-03-08 20:59:57 +0800

    通过添加dummy，让nanos的hello跑起来

--- abstract-machine/am/src/riscv/npc/ioe/ioe.c
+++ abstract-machine/am/src/riscv/npc/ioe/ioe.c
@@ -12,6 +12,8 @@ void __am_uart_tx(AM_UART_TX_T *);
 static void __am_timer_config(AM_TIMER_CONFIG_T *cfg) { cfg->present = true; cfg->has_rtc = true; }
 static void __am_input_config(AM_INPUT_CONFIG_T *cfg) { cfg->present = true;  }
 
+static inline void _dummy(AM_AUDIO_CONFIG_T *cfg) {}
+
 typedef void (*handler_t)(void *buf);
 static void *lut[128] = {
   [AM_TIMER_CONFIG] = __am_timer_config,
@@ -20,6 +22,18 @@ static void *lut[128] = {
   [AM_INPUT_CONFIG] = __am_input_config,
   [AM_INPUT_KEYBRD] = __am_input_keybrd,
   [AM_UART_TX]      = __am_uart_tx,
+  [AM_GPU_CONFIG  ] = _dummy,
+  [AM_GPU_FBDRAW  ] = _dummy,
+  [AM_GPU_STATUS  ] = _dummy,
+  [AM_UART_CONFIG ] = _dummy,
+  [AM_AUDIO_CONFIG] = _dummy,
+  [AM_AUDIO_CTRL  ] = _dummy,
+  [AM_AUDIO_STATUS] = _dummy,
+  [AM_AUDIO_PLAY  ] = _dummy,
+  [AM_DISK_CONFIG ] = _dummy,
+  [AM_DISK_STATUS ] = _dummy,
+  [AM_DISK_BLKIO  ] = _dummy,
+  [AM_NET_CONFIG  ] = _dummy,
 };
 
 static void fail(void *buf) { panic("access nonexist register"); }
